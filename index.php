0b4ff20ccc9048c7b63851d0ad4af8f92629706b
Farid <relampagolanzadel04@gmail.com>
2018-12-08 02:32:07 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- CUSTOM CSS -->
+  <link rel='stylesheet' href='./styles/style.css' />
+
+  <title>CourseBrothers | One stop destination for learning</title>
+</head>
+<body>
+  
+  <!-- MAIN CONTAINER -->
+  <div class="main-container">
+
+    <!-- HEADER -->
+    <div class='header'>
+      <h2>This is the header file</h2>
+    </div>
+    <!-- /HEADER -->
+
+  </div>
+  <!-- /MAIN CONTAINER -->
+
+</body>
+</html>
