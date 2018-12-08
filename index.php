af45b1c14569c638b03c172f6fcb9028f6a234ef
Farid <relampagolanzadel04@gmail.com>
2018-12-08 18:15:41 +0530

    Added header BG and contact bar

--- index.php
+++ index.php
@@ -23,8 +23,98 @@
 
     <!-- HEADER -->
     <div class='header'>
-      <p>This is the header file</p>
-      <button>Testing</button>
+
+      <!-- HEADER_CONTACT -->
+      <div class='header__contact'>
+        <div class="header__contact__container">
+          
+          <div class="contact">
+            <div class="contact__num">
+              <span>
+                <img src='assets\svg\Icons\white\phone-book.svg' />
+                <span>+91 7044356122</span>
+              </span>
+            </div>
+            <div class="contact__email">
+              <span>
+                <img src='assets\svg\Icons\white\mail-black-envelope-symbol.svg' />
+                <span>shyam.d@gmail.com</span>
+              </span>
+            </div>
+          </div>
+
+          <div class="features">
+            <div class="feature">
+              <span>
+                <span class='feature__text'>
+                  <span>CREDIBLE</span>
+                  <span>INSTITUTES</span>
+                </span>
+                <img src='assets\svg\Icons\white\apartments.svg' />
+              </span>
+            </div>
+            <div class="feature">
+              <span>
+                <span class='feature__text'>
+                  <span>TRANSPARENT</span>
+                  <span>INFORMATION</span>
+                </span>
+                <img src='assets\svg\Icons\white\information.svg' />
+              </span>
+            </div>
+            <div class="feature">
+              <span>
+                <span class='feature__text'>
+                  <span>GENUINE</span>
+                  <span>FEE STRUCTURE</span>
+                </span>
+                <img src='assets\svg\Icons\white\give-money.svg' />
+              </span>
+            </div>
+            <div class="feature">
+              <span>
+                <span class='feature__text'>
+                  <span>COUNSELLING</span>
+                  <span>SUPPORT</span>
+                </span>
+                <img src='assets\svg\Icons\white\phone-receiver.svg' />
+              </span>
+            </div>
+            <div class="feature">
+              <span>
+                <span class='feature__text'>
+                  <span>STUDY</span>
+                  <span>MATERIAL</span>
+                </span>
+                <img src='assets\svg\Icons\white\books-stack-of-three.svg' />
+              </span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <!-- /HEADER_CONTACT -->
+
+      <!-- MAIN_HEADER -->
+      <div class="header__main">
+
+        <!-- HEADER_CONTAINER -->
+        <div class="header__container">
+
+          <div class="header__logo">
+            <img src='assets\svg\cb-logo.svg' />
+          </div>
+
+          <div class="header__content">
+            
+          </div>
+
+        </div>
+        <!-- /HEADER_CONTAINER -->
+
+      </div>
+      <!-- /MAIN_HEADER -->
+
     </div>
     <!-- /HEADER -->
 
