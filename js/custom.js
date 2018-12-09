a5c9aa7d0a6ed74d273f76e267e1e06d3d4b172b
Farid <relampagolanzadel04@gmail.com>
2018-12-09 22:26:52 +0530

    Added deleted caret animation

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,37 @@
 $(document).ready(function() {
 
+  // =====  GSAP Animations =====
+
+  // Variables
+  $caret = $('.header__caret');
+
+  // caretAnimation
+  function caretAnim() {
+    const caretTl = new TimelineMax({ repeat: -1, yoyo: true });
+    caretTl
+      .fromTo($caret, 1.5, {
+        y: -35,
+        autoAlpha: 0,
+      }, {
+        y: '+=20',
+        autoAlpha: 1,
+        ease: Power1.easeInOut
+      })
+    return caretTl;
+  };
+
+    // Start function with Master Timeline
+  function start() {
+    console.log('----- GSAP STARTS -----');
+    const masterTl = new TimelineMax();
+    masterTl
+      .add(caretAnim(), 'caret-animation')
+    ;
+  };
+
+
   // WOW JS
-  window.addEventListener('load', function () { new WOW().init(); });
+  window.addEventListener('load', function () { start(); new WOW().init(); });
 
   // Add tooltips
   $('.tooltip').tooltipster({
