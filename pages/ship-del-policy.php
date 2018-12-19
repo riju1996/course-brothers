9259eca7fd0712d4392fe96016bda126493cd9ad
Farid <relampagolanzadel04@gmail.com>
2018-12-19 17:59:44 +0530

    Added policy pages

---
+++ pages/ship-del-policy.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="shortcut icon" href="..\assets\images\favicon.png" type="image/png" />
+
+  <!-- NORMALIZE CSS -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css' />
+
+  <!-- GOOGLE FONTS -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:100,300,400,500,600,700" rel="stylesheet">
+
+  <!-- FONTAWESOME -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' />
+
+  <!-- ANIMATE CSS -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css' />
+
+  <!-- CUSTOM CSS -->
+  <link rel='stylesheet' href='../styles/style.css' />
+
+  <title>CourseBrother.com | Delivery Policy</title>
+</head>
+<body>
+  
+  
+  <!-- MAIN_CONTAINER -->
+  <div class="main-container">
+
+
+    <!-- NAVBAR_SCROLL -->
+    <?php include '../includes/navbar-scroll.php' ?>
+    <!-- /NAVBAR_SCROLL -->
+
+
+    <!-- POLICY_CONTAINER -->
+    <div class="policy_container">
+
+      <!-- POLICY_HEADER -->
+      <div class="policy_header">
+        <h3>shipping and delivery policy</h3>
+      </div>
+      <!-- /POLICY_HEADER -->
+      
+    </div>
+    <!-- POLICY_CONTAINER -->
+    
+
+    <!-- FOOTER -->
+    <?php include '../includes/footer.php' ?>
+    <!-- /FOOTER -->
+
+
+
+  </div>
+  <!-- /MAIN_CONTAINER -->
+
+
+  <!-- jQuery -->
+  <script
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous">
+  </script>
+  <!-- WOW JS -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js'></script>
+
+</body>
+</html> 
